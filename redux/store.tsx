7bf32e71e15cd080732bb45bff71923b7c5009a9
yliaster1919 <oliver.visto@kodakollectiv.com>
2023-11-17 15:16:19 +0800

    fix: missing main store

--- redux/store.tsx
+++ redux/store.tsx
@@ -1,17 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit'
+import mainReducer from '@/app/[locale]/main/_redux/main-slice';
 import checkoutReducer from '@/app/[locale]/main/checkout/_redux/checkout-slice'
 import senderReducer from '@/app/[locale]/main/checkout/(pages)/sender/_redux/sender-slice'
 import receiverReducer from '@/app/[locale]/main/checkout/(pages)/receiver/_redux/receiver-slice'
 import paymentMethodReducer from '@/app/[locale]/main/checkout/(pages)/payment-method/_redux/payment-method-slice'
 export const store = configureStore({
     reducer: {
+        main: mainReducer,
         checkout: checkoutReducer,
         sender: senderReducer,
         receiver: receiverReducer,
         paymentMethod: paymentMethodReducer,
     }
 })
-
-
 export type StoreState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
