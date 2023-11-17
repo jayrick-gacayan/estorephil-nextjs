c5417307b3d5efb6ad75452c66c187edf61f9cfb
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2023-11-17 14:34:13 +0800

    fix: partial fix on the modal

--- app/[locale]/_components/modal.tsx
+++ app/[locale]/_components/modal.tsx
@@ -9,7 +9,7 @@ export default function Modal({
 }): JSX.Element {
 
   return (
-    <div className={`transition-all fixed top-0 left-0 w-screen h-screen ${open ? `z-[9999] duration-75` : `-z-[9999] duration-500`} flex items-center justify-center
+    <div className={`transition-all fixed z-[9999] top-0 left-0 w-screen h-screen ${open ? `block` : `hidden`} flex items-center justify-center
       after:bg-black/30 after:absolute after:top-0 after:left-0 after:w-screen after:h-screen after:z-0`}>
       {children}
     </div>
--- app/[locale]/main/_sections/product-delivery-address-modal.tsx
+++ app/[locale]/main/_sections/product-delivery-address-modal.tsx
@@ -4,7 +4,7 @@ import { useAppDispatch, useAppSelector } from '@/app/_hooks/redux_hooks';
 import Modal from '../../_components/modal';
 import { AppDispatch, StoreState } from '@/redux/store';
 import { MainState } from '../_redux/main_state';
-import { memo, useMemo, useRef } from 'react';
+import { memo, useEffect, useMemo, useRef } from 'react';
 import { onModalProductDeliveryAddressOpened } from '../_redux/main-slice';
 import { useOutsideClick } from '@/app/_hooks/use-outside-click';
 import EnterDeliveryAddress from './enter-delivery-address';
@@ -22,16 +22,39 @@ function ProductDeliveryAddressModal() {
 
   }, [mainState.modalProductDeliveryAddressInfo]);
 
+  useEffect(() => {
+
+    if (open) {
+      setTimeout(() => {
+        if (modalContentRef.current) {
+          modalContentRef.current.classList.add('bottom-0');
+          modalContentRef.current.classList.remove('-bottom-40');
+        }
+      }, 300);
+
+
+
+    }
+  }, [open])
 
   useOutsideClick(modalContentRef, () => { onClose(); });
 
-  function onClose(): void { dispatch(onModalProductDeliveryAddressOpened('')); }
+  function onClose(): void {
+    if (modalContentRef.current) {
+      if (open) {
+        modalContentRef.current.classList.remove('bottom-0');
+        modalContentRef.current.classList.add('-bottom-40');
+        setTimeout(() => {
+          dispatch(onModalProductDeliveryAddressOpened(''));
+        }, 300);
+      }
+    }
+  }
 
   return (
     <Modal open={open}>
       <div ref={modalContentRef}
-        className={`transition-all duration-200 ease-in-out md:w-[640px] flex-none w-auto rounded-2xl bg-white text-center relative z-10 
-        ${mainState.modalProductDeliveryAddressInfo.open ? 'bottom-0' : '-bottom-40'}`}>
+        className={`transition-all duration-200 ease-in-out md:w-[640px] flex-none w-auto rounded-2xl bg-white text-center relative z-10`}>
         {(open && type === 'enterAddress') && (<EnterDeliveryAddress />)}
         {(open && type === 'changeAddress') && (<ChangeAddress />)}
         {(open && type === 'shoppingMethod') && (<ShoppingMethod />)}
