b9fa1a1ce337e460ceab67fcd6699580306a34bb
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2023-11-17 15:30:42 +0800

    fix: fixed redux store state

--- redux/store.tsx
+++ redux/store.tsx
@@ -1,11 +1,14 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore } from '@reduxjs/toolkit';
+import mainReducer from '@/app/[locale]/main/_redux/main-slice';
 import checkoutReducer from '@/app/[locale]/main/checkout/_redux/checkout-slice'
 import senderReducer from '@/app/[locale]/main/checkout/(pages)/sender/_redux/sender-slice'
 import receiverReducer from '@/app/[locale]/main/checkout/(pages)/receiver/_redux/receiver-slice'
 import paymentMethodReducer from '@/app/[locale]/main/checkout/(pages)/payment-method/_redux/payment-method-slice'
+
 export const store = configureStore({
     reducer: {
         checkout: checkoutReducer,
+        main: mainReducer,
         sender: senderReducer,
         receiver: receiverReducer,
         paymentMethod: paymentMethodReducer,
