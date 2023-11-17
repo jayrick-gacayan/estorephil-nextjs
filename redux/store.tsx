3524bdcc26855c7e92c333c94ecab365fe3e5bc5
yliaster1919 <oliver.visto@kodakollectiv.com>
2023-11-17 15:40:58 +0800

    commit

--- redux/store.tsx
+++ redux/store.tsx
@@ -14,6 +14,5 @@ export const store = configureStore({
         paymentMethod: paymentMethodReducer,
     }
 })
-
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
