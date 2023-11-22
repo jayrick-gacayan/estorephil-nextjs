32961e3950e33b3382ca52603a4eae03a47944ab
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2023-11-22 14:46:19 +0800

    feat: implemented pointer or mouse events on carousel

--- app/[locale]/main/home/_sections/carousel.tsx
+++ app/[locale]/main/home/_sections/carousel.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useEffect, useRef, useState, MouseEvent, TouchEvent } from 'react';
+import { useEffect, useRef, useState, MouseEvent, TouchEvent, useCallback } from 'react';
 import { CarouselItem } from '../_components/carousel-item';
 import { FaChevronRight, FaChevronLeft } from 'react-icons/fa';
 import { CarouselItemProps } from '@/types/props/carousel-item-props';
@@ -17,60 +17,55 @@ export function Carousel() {
   const carouselIntervalRef = useRef<any>(null);
   const [carouselCurrentSlide, setCarouselCurrentSide] = useState<number>(0);
 
+  const carouselMoveToRight = useCallback(() => {
+    setCarouselCurrentSide(carouselCurrentSlide === (carouselItems.length - 1) ? 0 : carouselCurrentSlide + 1)
+  }, [setCarouselCurrentSide, carouselCurrentSlide]);
+
+  const carouselMoveToLeft = useCallback(() => {
+    setCarouselCurrentSide(carouselCurrentSlide === 0 ? (carouselItems.length - 1) : carouselCurrentSlide - 1);
+  }, [setCarouselCurrentSide, carouselCurrentSlide])
+
+  const clearCarouselInterval = useCallback(() => {
+    clearInterval(carouselIntervalRef.current)
+    carouselIntervalRef.current = null;
+  }, [carouselIntervalRef])
+
   useEffect(() => {
     carouselIntervalRef.current = setInterval(() => {
-      carouselMoveRightIndex();
+      carouselMoveToRight();
     }, 3000);
 
     return () => {
-      clearIntervalCarousel();
+      clearCarouselInterval();
     }
-  }, [carouselCurrentSlide]);
+  }, [carouselMoveToRight]);
 
 
   function handlePointerEvent(event: MouseEvent | TouchEvent) {
-    clearIntervalCarousel();
+    clearCarouselInterval();
 
     const card = event.target as HTMLDivElement;
     let offset = 0;
     const isTouchEvent = event.type === "touchstart";
 
     const initialX = isTouchEvent ? (event as TouchEvent).touches[0].clientX : (event as MouseEvent).clientX;
-
-
     document.addEventListener('mousemove', onPointerMove as any);
     document.addEventListener('mouseup', onPointerEnd);
     document.addEventListener('touchmove', onPointerMove as any);
     document.addEventListener('touchend', onPointerEnd);
 
     function onPointerMove(moveEvent: MouseEvent | TouchEvent) {
-
       offset = (isTouchEvent ? (moveEvent as TouchEvent).touches[0].clientX : (moveEvent as MouseEvent).clientX) - initialX;
 
-      console.log('offset', offset);
-      carouselMoveRightIndex();
-      if (offset <= -50) {
-        if (carouselCurrentSlide === carouselItems.length - 1) {
-          card.style.left = "0px";
-        } else {
-          setTimeout(() => {
-            card.style.left = "0px";
-          }, 500);
-        }
-
+      if (offset <= -200) {
+        carouselMoveToRight();
+        card.style.left = "0px";
         return;
       }
 
-      if (offset >= 50) {
-        if (carouselCurrentSlide === 0) {
-          card.style.left = "0px";
-        } else {
-          setTimeout(() => {
-            card.style.left = "0px";
-          }, 500);
-        }
-
-        carouselMoveLeftIndex();
+      if (offset >= 200) {
+        carouselMoveToLeft();
+        card.style.left = "0px";
         return;
       }
 
@@ -78,10 +73,10 @@ export function Carousel() {
     }
 
     function onPointerEnd() {
-      if (offset < 0 && offset > -100) {
+      if (offset < 0 && offset > -200) {
         card.style.left = "0px";
       }
-      if (offset > 0 && offset < 100) {
+      if (offset > 0 && offset < 200) {
         card.style.left = "0px";
       }
 
@@ -91,37 +86,21 @@ export function Carousel() {
       document.removeEventListener('touchend', onPointerEnd);
     }
   }
-  function carouselMoveLeftIndex() {
-    setCarouselCurrentSide((currentSlide: number) => {
-      return currentSlide === 0 ? (carouselItems.length - 1) : (currentSlide = currentSlide - 1);
-    })
-  }
-
-  function carouselMoveRightIndex() {
-    setCarouselCurrentSide((currentSlide: number) => {
-      return currentSlide === (carouselItems.length - 1) ? 0 : (currentSlide = currentSlide + 1);
-    })
-  }
-
-  function clearIntervalCarousel() {
-    clearInterval(carouselIntervalRef.current)
-    carouselIntervalRef.current = null;
-  }
 
   return (
     <div className='flex-1 h-full w-full'>
       <div className='relative h-full w-full overflow-hidden'>
         <div className='absolute top-[50%] left-[12px] z-20 text-white/100 cursor-pointer'
           onClick={() => {
-            clearIntervalCarousel();
-            carouselMoveLeftIndex();
+            clearCarouselInterval();
+            carouselMoveToLeft();
           }}>
           <FaChevronLeft className='w-6 h-6 block text-white/100' />
         </div>
         <div className='absolute top-[50%] right-[12px] z-20 text-white/100 cursor-pointer'
           onClick={() => {
-            clearIntervalCarousel();
-            carouselMoveRightIndex();
+            clearCarouselInterval();
+            carouselMoveToRight();
           }}>
           <FaChevronRight className='w-6 h-6 block' />
         </div>
@@ -134,8 +113,7 @@ export function Carousel() {
                   imgAlt={`carousel-img-${carouselItem.index + 1}`}
                   index={carouselItem.index}
                   currentSlide={carouselCurrentSlide}
-                  onPointerEvent={handlePointerEvent}
-                />
+                  onPointerEvent={handlePointerEvent} />
               )
             })
           }
