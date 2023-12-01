083216d2abf3b1d67348177153ad8f4085078b87
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2023-12-01 11:59:49 +0800

    feat: partial implementation of rating with functionalities

---
+++ app/[locale]/_components/rating.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { useState, useRef, MouseEvent } from 'react';
+import { FaRegStar, FaStar } from 'react-icons/fa6';
+
+export default function Rating({
+  id,
+  rating,
+}: {
+  id: number;
+  rating: number;
+}) {
+  const [activeStar, setActiveStar] = useState<number>(rating);
+  const [hoverActiveStar, setHoverActiveStar] = useState(-1);
+  const [isHovered, setIsHovered] = useState(false);
+  const ratingContainerRef = useRef<HTMLDivElement>(null);
+
+  function calculateRating(event: MouseEvent) {
+    let numberInStars = 0;
+
+    if (ratingContainerRef.current) {
+      const { width, left } = ratingContainerRef.current.getBoundingClientRect();
+      let percent = (event.clientX - left) / width;
+      numberInStars = percent * 5;
+    }
+
+    return Math.round(numberInStars * 100) / 100;
+  }
+
+  function handleRatingOnClick(event: MouseEvent<HTMLDivElement>) {
+    setActiveStar(calculateRating(event));
+  }
+
+  function handleRatingMouseMove(event: MouseEvent<HTMLDivElement>) {
+    setIsHovered(true);
+    setHoverActiveStar(calculateRating(event));
+  };
+
+  function handleMouseLeave(event: MouseEvent<HTMLDivElement>) {
+    setHoverActiveStar(-1); // Reset to default state
+    setIsHovered(false);
+  };
+
+  return (
+    <div ref={ratingContainerRef}
+      className='inline-flex relative cursor-pointer text-left'
+      onClick={handleRatingOnClick}
+      onMouseMove={handleRatingMouseMove}
+      onMouseLeave={handleMouseLeave}>
+      {
+        [...new Array(5)].map((value: any, index: number) => {
+
+          const activeState = isHovered ? hoverActiveStar : activeStar;
+          const showEmptyIcon = activeState < (index + 1);
+
+          const isActiveRating = activeState !== 1;
+          const isRatingWithPrecision = activeState % 1 !== 0;
+          const isRatingEqualToIndex = Math.ceil(activeState) === index + 1;
+          const showRatingWithPrecision = isActiveRating && isRatingWithPrecision && isRatingEqualToIndex;
+          return (
+            <div key={`product-${id}-${index}`}
+              className='relative cursor-pointer inline text-[#FAD115]'>
+              <div className={`absolute overflow-hidden z-10`}
+                style={{ width: showRatingWithPrecision ? `${(activeState % 1) * 100}%` : `0%` }}>
+                <FaStar />
+              </div>
+              <div className='relative'>
+                {showEmptyIcon ? <FaRegStar /> : <FaStar />}
+              </div>
+            </div>
+          )
+        })
+      }
+    </div>
+  )
+}
--- app/[locale]/main/_components/product-item.tsx
+++ app/[locale]/main/_components/product-item.tsx
@@ -1,7 +1,7 @@
 import { Product } from '@/models/product';
 import Image from 'next/image';
-import Link from 'next-intl/link'; import { FaRegStar, FaStar } from 'react-icons/fa6';
-0
+import Link from 'next-intl/link';
+import Rating from '../../_components/rating';
 
 export function ProductItem({
   product,
@@ -10,8 +10,7 @@ export function ProductItem({
 }) {
 
   return (
-    <Link href={`/products/${product.id}`}
-      className='w-60 bg-white border border-neutral-200 rounded overflow-hidden cursor-pointer space-y-4'>
+    <div className='w-60 bg-white border border-neutral-200 rounded overflow-hidden cursor-pointer space-y-4'>
       <div className=' w-full h-60 overflow-hidden'>
         <div className='transition-all delay-200 ease-in hover:scale-125 h-full'>
           <Image alt={`product-image-${product.id}`}
@@ -22,58 +21,10 @@ export function ProductItem({
         </div>
       </div>
       <div className='p-2'>
-        <div className='font-bold'>{product.name}</div>
+
+        <Link href={`/products/${product.id}`} className='font-bold block hover:text-primary'>{product.name}</Link>
         <div className='block space-x-1'>
-          <div className='inline-flex relative cursor-pointer align-middle'>
-            {
-              [...new Array(5)].map((value: any, index: number) => {
-
-                const activeState = product.rating;
-                const showEmptyIcon = activeState < (index + 1);
-                console.log('dsfjsdfd', index, activeState, showEmptyIcon, product.rating)
-
-                const isActiveRating = activeState !== 1;
-                const isRatingWithPrecision = activeState % 1 !== 0;
-                const isRatingEqualToIndex = Math.ceil(activeState) === index + 1;
-                const showRatingWithPrecision = isActiveRating && isRatingWithPrecision && isRatingEqualToIndex;
-                return (
-                  <div key={`product-${product.id}-${index}`}
-                    className='relative cursor-pointer inline text-[#FAD115]'>
-                    <div className={`absolute overflow-hidden z-10`}
-                      style={{ width: showRatingWithPrecision ? `${(activeState % 1) * 100}%` : `0%` }}>
-                      <FaStar />
-                    </div>
-                    <div className={`relative`}>
-                      {showEmptyIcon ? <FaRegStar /> : <FaStar />}
-                    </div>
-                  </div>
-                )
-              })
-            }
-
-            {/* <div className='block absolute z-10 top-0 left-0 overflow-hidden'>
-              {
-                Array.from({ length: 5 }, (_value, index: number) => {
-                  return index + 1;
-                }).map((value: number) => {
-                  return <FaStar className="inline align-middle"
-                    style={{
-                      width: rating
-                    }} />
-                })
-              }
-            </div> */}
-            {/* <div className='relative flex top-0 left-0 text-[20px] overflow-hidden text-[#FAD115] z-10'
-              style={{ width: Math.round((product.rating / 5) * 80) }}>
-              {
-                Array.from({ length: 5 }, (_value, index: number) => {
-                  return index + 1;
-                }).map((value: number) => {
-                  return <FaStar size={24} className="inline" />
-                })
-              }
-            </div> */}
-          </div>
+          <Rating id={product.id} rating={product.rating} />
           <span className='inline-block align-middle'>&#40;{product.raters}&#41;</span>
         </div>
 
@@ -81,6 +32,6 @@ export function ProductItem({
           &#8369; {product.price.toFixed(2)}
         </div>
       </div>
-    </Link>
+    </div>
   )
 }
