ddd1c60b7539f365753b3a28c3f3f2ca85c7000c
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2023-12-01 15:13:45 +0800

    fix: added hover on floating button

--- app/[locale]/main/layout.tsx
+++ app/[locale]/main/layout.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 import { Footer } from './_sections/footer';
 import MainHeader from './_sections/main-header';
 import { MobileStickyFooter } from './_sections/mobile-sticky-footer';
-import FloatingCardButton from './_sections/floating-shop-method-button';
+import FloatingCardButton from './_sections/floating-purchase-method-button';
 import MainLayoutModal from './_sections/main-layout-modal';
 
 export default function Layout({ children }: { children: ReactNode }) {
