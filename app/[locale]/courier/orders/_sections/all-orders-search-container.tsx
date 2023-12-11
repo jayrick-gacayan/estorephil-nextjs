00029f2c8d3695f5e7590c3f5a068a72b38bac58
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2023-12-11 12:20:56 +0800

    feat: all orders page screen

---
+++ app/[locale]/courier/orders/_sections/all-orders-search-container.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import OrdersSearchContainer from "../_components/orders-search-container";
+
+export default function AllOrdersSearchContainer() {
+  return (<OrdersSearchContainer />)
+}
