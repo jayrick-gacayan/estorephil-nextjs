47857e55f6aac34c676a92587cc8066307367b27
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2023-12-14 14:57:44 +0800

    feat: added admin dashboard pages

--- app/[locale]/(dashboard)/admin/_sections/admin-sidebar.tsx
+++ app/[locale]/(dashboard)/admin/_sections/admin-sidebar.tsx
@@ -7,13 +7,85 @@ import { useMemo, useState, MouseEvent, memo } from 'react';
 import { useRouter } from 'next-intl/client';
 import DashboardMenuItemWithSubLinks from '../../_components/dashboard-menu-item-with-sublinks';
 import DashboardMenuItemLink from '../../_components/dashboard-menu-item-link';
+import { FaShoppingBasket, FaShoppingCart } from 'react-icons/fa';
+import { FaChartBar, FaChartLine, FaFilter, FaImages, FaMoneyCheck, FaUsers } from 'react-icons/fa6';
 
-let courierDashboardMenus: MenuProps[] = [
+let adminDashboardMenus: MenuProps[] = [
   {
-    link: '/courier',
+    link: '/admin/dashboard',
     text: 'Dashboard',
     alt: 'dashboard',
     icon: <BiSolidDashboard />
+  },
+  {
+    link: '/admin/products',
+    text: 'Products',
+    alt: 'products',
+    icon: <FaShoppingCart />
+  },
+  {
+    link: '/admin/tax-rules',
+    alt: 'tax-rules',
+    text: 'Tax Rules',
+    icon: <FaMoneyCheck />
+  },
+  {
+    link: '/admin/trade_rates',
+    alt: 'trade-rates',
+    text: 'Trade Rates',
+    icon: <FaChartBar />
+  },
+  {
+    link: '/admin/categories',
+    alt: 'categories',
+    text: 'Categories',
+    icon: <FaFilter />
+  },
+  {
+    link: '/admin/accounting',
+    alt: 'accounting',
+    text: 'Accounting',
+    icon: <FaChartLine />
+  },
+  {
+    link: '/admin/banners',
+    alt: 'banners',
+    text: 'Banners',
+    icon: <FaImages />
+  },
+  {
+    link: '/admin/orders',
+    alt: 'orders',
+    text: 'Orders',
+    icon: <FaShoppingBasket />
+  },
+  {
+    link: '/admin/users',
+    text: 'Users',
+    alt: 'users',
+    icon: <FaUsers />,
+    subMenus: [
+      {
+        link: '/admin/users/admins',
+        text: 'Admins',
+        alt: 'admins',
+      },
+      {
+        link: '/admin/users/agent',
+        text: 'Agents',
+        alt: 'agents'
+      },
+      {
+        link: '/admin/users/couriers',
+        text: 'Couriers',
+        alt: 'couriers'
+      },
+      {
+        link: '/admin/users/sellers',
+        text: 'Sellers',
+        alt: 'sellers'
+      }
+    ]
   },
 ];
 
@@ -38,50 +110,50 @@ function AdminSidebar() {
         <div className='px-4 py-2'>Menu</div>
         <div className='block'>
           {
-            courierDashboardMenus.map((courierDashboardMenu: MenuProps) => {
+            adminDashboardMenus.map((adminDashboardMenu: MenuProps) => {
 
-              if (courierDashboardMenu.subMenus) {
-                let objectToCourierMenuItemWithSubLinksProps: any = courierDashboardMenu;
-                if (courierDashboardMenu.link) {
-                  objectToCourierMenuItemWithSubLinksProps = {
-                    ...objectToCourierMenuItemWithSubLinksProps,
+              if (adminDashboardMenu.subMenus) {
+                let objectToAdminMenuItemWithSubLinksProps: any = adminDashboardMenu;
+                if (adminDashboardMenu.link) {
+                  objectToAdminMenuItemWithSubLinksProps = {
+                    ...objectToAdminMenuItemWithSubLinksProps,
                     currentDropdownMenu: currentDropdownMenu,
                     currentDropdownMenuSet: (currentMenu: string) => {
-                      setCurrentDropdownMenu(currentDropdownMenu === currentMenu ? '' : courierDashboardMenu.alt)
+                      setCurrentDropdownMenu(currentDropdownMenu === currentMenu ? '' : adminDashboardMenu.alt)
                     },
                     onLinkClicked: () => {
-                      router.push(courierDashboardMenu.link!);
+                      router.push(adminDashboardMenu.link!);
                     }
                   }
                 }
 
                 return (<DashboardMenuItemWithSubLinks segment={segment}
-                  key={`menu-sublink-items-${courierDashboardMenu.text}`}
+                  key={`menu-sublink-items-${adminDashboardMenu.text}`}
                   onActiveMenu={function (alt: string, segment: string): string {
                     return segment === alt ? `text-primary bg-tertiary-light` : ``;
                   }}
-                  {...objectToCourierMenuItemWithSubLinksProps}>
-                  {courierDashboardMenu.alt === 'orders' && <div className='rounded-full w-auto bg-danger text-white px-1.5 inline-block text-sm'>4</div>}
+                  {...objectToAdminMenuItemWithSubLinksProps}>
+                  {adminDashboardMenu.alt === 'orders' && <div className='rounded-full w-auto bg-danger text-white px-1.5 inline-block text-sm'>4</div>}
                 </DashboardMenuItemWithSubLinks>)
               }
 
-              let objectToCourierMenuItemLinkProps: any = courierDashboardMenu;
+              let objectToAdminMenuItemLinkProps: any = adminDashboardMenu;
 
-              if (courierDashboardMenu.alt === 'delivery-rates') {
-                objectToCourierMenuItemLinkProps = {
-                  ...objectToCourierMenuItemLinkProps,
+              if (adminDashboardMenu.alt === 'delivery-rates') {
+                objectToAdminMenuItemLinkProps = {
+                  ...objectToAdminMenuItemLinkProps,
                   onDisabledLink: (event: MouseEvent<HTMLAnchorElement>) => {
                     event.preventDefault();
                   }
                 }
               }
 
-              return (<DashboardMenuItemLink key={`menu-items-${courierDashboardMenu.text}`}
+              return (<DashboardMenuItemLink key={`menu-items-${adminDashboardMenu.text}`}
                 segment={segment!}
                 onActiveMenu={(alt: string, segment: string) => {
                   return segment === alt ? 'text-primary border-l-4 border-l-primary bg-tertiary-light' : ''
                 }}
-                {...objectToCourierMenuItemLinkProps} />)
+                {...objectToAdminMenuItemLinkProps} />)
             })
           }
         </div>
