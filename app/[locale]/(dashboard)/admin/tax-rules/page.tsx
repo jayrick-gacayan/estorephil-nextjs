8e381b4e8e889519ab95cd6abb74a821d12a5fd6
jayrick-gacayan <jayrick.gacayan@kodakollectiv.com>
2023-12-14 15:18:56 +0800

    fix: fixed not found page

--- app/[locale]/(dashboard)/admin/tax-rules/page.tsx
+++ app/[locale]/(dashboard)/admin/tax-rules/page.tsx
@@ -2,7 +2,7 @@ import DashboardHeaderText from "../../_components/dashboard-header-text";
 
 export default function Page() {
   return <>
-    <DashboardHeaderText text="Tax Rates" />
+    <DashboardHeaderText text="Tax Rules" />
     <div className='bg-[#F5F7FA] p-4 space-y-4 flex-1 overflow-auto'>
 
     </div>
