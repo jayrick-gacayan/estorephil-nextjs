01f93750a358a27074704d324c158eb6e8cdc134
Jofel Salado <jofel.salado@kodakollectiv.com>
2023-12-14 15:46:02 +0800

    feat: added contents to each admin dashboard pages

--- app/[locale]/(dashboard)/admin/products/page.tsx
+++ app/[locale]/(dashboard)/admin/products/page.tsx
@@ -1,10 +1,13 @@
 import DashboardHeaderText from "../../_components/dashboard-header-text";
+import ProductItems from "./_sections/product-items";
 
 export default function Page() {
-  return <>
-    <DashboardHeaderText text="Products" />
-    <div className='bg-[#F5F7FA] p-4 space-y-4 flex-1 overflow-auto'>
-
-    </div>
-  </>
+  return (
+    <>
+      <DashboardHeaderText text="Products" />
+      <div className="bg-[#F5F7FA] p-4 space-y-4 flex-1 overflow-auto">
+        <ProductItems />
+      </div>
+    </>
+  );
 }
