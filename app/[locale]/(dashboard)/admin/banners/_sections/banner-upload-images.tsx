09a95b73a391c0fd7e3a3ee58d728dfe6710443e
Jofel Salado <jofel.salado@kodakollectiv.com>
2023-12-14 17:35:50 +0800

    feat: added content for admin banner

---
+++ app/[locale]/(dashboard)/admin/banners/_sections/banner-upload-images.tsx
@@ -0,0 +1,5 @@
+import BannerInput from "./banner-input";
+
+export default function BannerUploadImages() {
+  return <div>asd</div>;
+}
--- app/[locale]/(dashboard)/admin/banners/page.tsx
+++ app/[locale]/(dashboard)/admin/banners/page.tsx
@@ -1,10 +1,17 @@
 import DashboardHeaderText from "../../_components/dashboard-header-text";
+import BannerInput from "./_sections/banner-input";
+import BannerUploadImages from "./_sections/banner-upload-images";
 
 export default function Page() {
-  return <>
-    <DashboardHeaderText text="Banners" />
-    <div className='bg-[#F5F7FA] p-4 space-y-4 flex-1 overflow-auto'>
-
-    </div>
-  </>
+  return (
+    <>
+      <DashboardHeaderText text="Banners" />
+      <div className="bg-[#F5F7FA] p-4 space-y-4 flex-1 overflow-auto">
+        <div>
+          <BannerInput />
+          <BannerUploadImages />
+        </div>
+      </div>
+    </>
+  );
 }
