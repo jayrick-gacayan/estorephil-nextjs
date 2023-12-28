fbc1232de28cd31430913a4c0b48861730e06e00
Jofel Salado <jofel.salado@kodakollectiv.com>
2023-12-28 16:03:26 +0800

    fix: added invite courier button and dialog

--- app/[locale]/(dashboard)/admin/users/couriers/_sections/courier-header.tsx
+++ app/[locale]/(dashboard)/admin/users/couriers/_sections/courier-header.tsx
@@ -4,7 +4,7 @@ import DashboardHeaderText from "@/app/[locale]/(dashboard)/_components/dashboar
 import DashboardHeaderTextButton from "@/app/[locale]/(dashboard)/_components/dashboard-header-text-button";
 import { useState } from "react";
 
-export default function AdminsHeader() {
+export default function CourierHeader() {
   const [isInviteDialogVisible, setIsInviteDialogVisible] = useState(false);
   const openInviteDialog = () => {
     setIsInviteDialogVisible(true);
@@ -17,9 +17,9 @@ export default function AdminsHeader() {
     <>
       {isInviteDialogVisible && (
         <div className="z-[999] fixed top-0 left-0 w-screen h-screen flex items-center justify-center bg-gray-800 bg-opacity-50">
-          <div className="flex flex-col justify-between bg-white h-[37.5rem] rounded-md w-[40rem] items-center p-8 gap-2">
+          <div className="flex flex-col justify-between bg-white h-[30.5rem] rounded-md w-[40rem] items-center p-8 gap-2">
             <div className="flex flex-col items-center gap-5  flex-1 w-[40rem] px-5">
-              <h2 className="text-xl font-bold mb-4">Invite Admin User</h2>
+              <h2 className="text-xl font-bold mb-4">Add Courier</h2>
               <div className="w-full flex flex-col  ">
                 <label className="block text-sm font-medium text-gray-700">
                   First Name:
@@ -29,15 +29,7 @@ export default function AdminsHeader() {
                   className="mt-1 p-2 border focus:border-blue-500 focus:outline-none w-full"
                 />
               </div>
-              <div className="w-full">
-                <label className="block text-sm font-medium text-gray-700">
-                  Last Name:
-                </label>
-                <input
-                  type="text"
-                  className="mt-1 p-2 border focus:border-blue-500 focus:outline-none w-full"
-                />
-              </div>
+
               <div className="w-full">
                 <label className="block text-sm font-medium text-gray-700">
                   Email:
@@ -60,6 +52,7 @@ export default function AdminsHeader() {
                   className="mt-1 p-2 border focus:border-blue-500 focus:outline-none w-full"
                 >
                   <option value="admin">Admin</option>
+                  <option value="admin">Manager</option>
                   <option value="staff">Staff</option>
                 </select>
               </div>
@@ -81,9 +74,9 @@ export default function AdminsHeader() {
           </div>
         </div>
       )}
-      <DashboardHeaderText text="Admins">
+      <DashboardHeaderText text="Courier">
         <DashboardHeaderTextButton
-          labelText="Invite Admin User"
+          labelText="Invite Courier"
           onClick={openInviteDialog}
         />
       </DashboardHeaderText>
